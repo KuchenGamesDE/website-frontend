85cd4db71296cd1ad56080fd578f2ed64d22aeb1
SMIBII <54860471+smibii@users.noreply.github.com>
2023-08-07 19:23:57 +0200

    Hover role name

--- src/components/Team/TeamCards.jsx
+++ src/components/Team/TeamCards.jsx
@@ -24,17 +24,31 @@ export default function TeamCards({ members, roles }) {
                     <div
                         title={role_name}
                         key={name}
-                        className="w-[120px] m-2 flex flex-col items-center bg-[#80808063] cursor-pointer transition rounded-md hover:text-[white]"
-                        onMouseEnter={e => {setIsHovered(true)}}
-                        onMouseLeave={e => {setIsHovered(false)}}
+                        className="relative w-[120px] transition-all m-2 flex flex-col items-center bg-[#5d5e61] cursor-pointer rounded-md hover:text-[white]"
+                        onMouseEnter={() => {setIsHovered(true)}}
+                        onMouseLeave={() => {setIsHovered(false)}}
                         style={{
                             background: isHovered
                                 ? role_color
-                                : "#80808063"
+                                : "#5d5e618d",
+                            height: isHovered
+                                ? "120px"
+                                : "140px",
+                            marginBottom: isHovered
+                                ? "20px"
+                                : "0"
                         }}
                     >
                         <div
-                            className="px-5 pt-3 w-[120px]"
+                            className="px-5 pt-3 transition-all"
+                            style={{
+                                width: isHovered
+                                    ? "100px"
+                                    : "120px",
+                                opacity: isHovered
+                                    ? "0.7"
+                                    : "1"
+                            }}
                         >
                             <img src={`${skin_api_uri}${uuid}`} alt="" />
                         </div>
@@ -42,6 +56,23 @@ export default function TeamCards({ members, roles }) {
                             className="font-bold text-sm pb-2 pt-3"
                         >
                             {name}
+                        </div>
+                        <div
+                            className="absolute w-full h-full flex justify-center"
+                        >
+                            <div
+                                className="font-bold text-sm mt-auto transition-all px-2 rounded-b-md"
+                                style={{
+                                    translate: isHovered
+                                        ? "0 20px"
+                                        : "0 0px",
+                                    color: isHovered
+                                        ? "white"
+                                        : "transparent"
+                                }}
+                            >
+                                {role_name}
+                            </div>
                         </div>
                     </div>
                 )
