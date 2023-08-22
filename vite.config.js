234c873b484d42d038b7c63285a792397709f9a4
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2023-08-22 05:29:06 +0200

    FEATURE start impl of serviceWorkers for PUSH

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: ``
+    base: ``,
+    plugins: [
+        react(),
+        VitePWA({
+            injectRegister: 'auto',
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg'],
+            importScripts: ['./src/serviceWorker.js'],
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5 MB max file bytes pre caching
+            }
+        })
+    ]
 });
