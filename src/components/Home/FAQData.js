4b53a390e61815642064c057eb4a8046fc5f621a
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2023-09-04 19:46:24 +0200

    FAQ Update

--- src/components/Home/FAQData.js
+++ src/components/Home/FAQData.js
@@ -44,53 +44,5 @@ mit allen wichtigen Informationen zu unserem Server veröffentlicht. Dort findes
 </a>
 eröffnen und das Support Team wird dir weiter helfen.
         `
-    },
-    {
-        title: 'Wie erhalte ich ein Grundstück in der Plotwelt?',
-        content: `
-Du kannst ein Grundstück erhalten, indem du den Befehl "/plot auto" in den Chat eingibst. Dir wird automatisch ein freies Grundstück zugewiesen.
-        `
-    },
-    {
-        title: 'Was sind ChestShops?',
-        content: `
-ChestShops sind virtuelle Läden, die du auf deinem Plot oder in anderen Welten erstellen kannst. Du kannst Waren in Kisten legen und Preise festlegen, damit andere Spieler bei dir einkaufen können.
-        `
-    },
-    {
-        title: 'Was ist der BattlePass?',
-        content: `
-Der BattlePass ist ein Fortschrittssystem, das es Spielern ermöglicht, Belohnungen zu verdienen, indem sie Herausforderungen abschließen. Je mehr Herausforderungen du abschließt, desto höher steigst du im BattlePass auf und erhältst bessere Belohnungen.
-        `
-    },
-    {
-        title: 'Wie funktioniert das RTP-System in der Farmwelt?',
-        content: `
-Das RTP-System (Random Teleport) ermöglicht es dir, zufällig in der Farmwelt zu teleportieren, um neue Gebiete zu erkunden. Du kannst den Befehl "/rtp go" verwenden, um dich an einen zufälligen Ort zu teleportieren.
-        `
-    },
-    {
-        title: 'Was sind Pets?',
-        content: `
-Pets sind Begleittiere, die du auf dem Server haben kannst. Sie können dir Gesellschaft leisten und in einigen Fällen besondere Fähigkeiten oder Boni bieten.
-        `
-    },
-    {
-        title: 'Was sind AdminShops?',
-        content: `
-AdminShops sind Läden, die von den Serveradministratoren verwaltet werden. Hier kannst du bestimmte Gegenstände kaufen oder verkaufen, um deine Ressourcen zu verwalten.
-        `
-    },
-    {
-        title: 'Was ist der Dungeon und was erwartet mich dort?',
-        content: `
-Der Dungeon ist ein spezieller Bereich auf dem Server, der Abenteuer und Herausforderungen bietet. Dort findest du spannende Kämpfe gegen Monster, einzigartige Beute und möglicherweise sogar eine kleine Hintergrundgeschichte, die das Spielerlebnis vertieft.
-        `
-    },
-    {
-        title: 'Was ist die Mine und was erwartet mich dort?',
-        content: `
-Die Mine ist ein Bereich auf dem Server, in dem du Rohstoffe abbauen kannst. Dort findest du verschiedene Ressourcen wie Erze und Edelsteine. Je tiefer du in die Mine gehst, desto wertvollere Materialien wirst du finden. Sei jedoch vorsichtig, da es auch Gefahren in den Tiefen der Mine geben kann.
-        `
     }
 ];
